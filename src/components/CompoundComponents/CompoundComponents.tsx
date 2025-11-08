8f5b81871807f0305afc2e3d8a2702acf1c1a885
juanlopcon07 <juan.21futmas07@gmail.com>
2025-11-08 16:30:58 -0500

    Patron compound components en react

--- src/components/CompoundComponents/CompoundComponents.tsx
+++ src/components/CompoundComponents/CompoundComponents.tsx
@@ -1,6 +1,18 @@
+import Tabs from "./Tabs";
+import Tab from "./Tab";
+
 export const CompoundComponents = () => {
   return (
-    <>
-    </>
+    <Tabs>
+      <Tab label="Girafa">
+        🦒
+      </Tab>
+      <Tab label="Delfin">
+        🐬
+      </Tab>
+      <Tab label="Cocodrilo">
+        🐊
+      </Tab>
+    </Tabs>
   );
 };
